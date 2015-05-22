d24d2461fe55c6c01ab2b42380217be3b852e555
Christopher Moore <MrMepper@gmail.com>
2015-05-22 12:03:00 -0500

    Added feature: User inputs state(TX,AR,etc)

--- LocuAPI.py
+++ LocuAPI.py
@@ -10,21 +10,24 @@ url = 'https://api.locu.com/v1_0/venue/search/?name=hello&locality=Conway&cuisin
 
 object_dict = {}
 
+state = input("Please enter your State:")
+object_dict['State'] = state
+
 locality = input("Please enter City Name:")
 object_dict['City'] = locality
 
 print("Would you like to enter the name of the restaurant?")
-choice = input('Y/y for yes or N/n for no')
+choice = input('Y/y for yes or N/n for no: ')
 if choice == 'Y' or choice == 'y':
-    rest_name = input("Please enter name of restaurant")
+    rest_name = input("Please enter name of restaurant: ")
 else:
     rest_name = None
 object_dict['Restaurant'] = rest_name
 
 print("Would you like to enter the cuisine of the restaurant?")
-choice = input('Y/y for yes or N/n for no')
+choice = input('Y/y for yes or N/n for no: ')
 if choice == 'Y' or choice == 'y':
-    food_type = input("Please enter desired cuisine")
+    food_type = input("Please enter desired cuisine: ")
 else:
     food_type = None
 object_dict['Cuisine'] = food_type
@@ -32,7 +35,7 @@ object_dict['Cuisine'] = food_type
 def restaurant_search(**object_dict):
     new_url = 'https://api.locu.com/v1_0/venue/search/?api_key=4fda38a894269e7abed0b1b5c4af272a4c82decd'
     new_url = 'https://api.locu.com/v1_0/venue/search/?api_key=4fda38a894269e7abed0b1b5c4af272a4c82decd' \
-        + "&locality=" + object_dict['City']
+        + "&locality=" + object_dict['City'] + '&region=' + object_dict['State']
 
     if object_dict['Restaurant'] is not None:
         new_url = 'https://api.locu.com/v1_0/venue/search/?api_key=4fda38a894269e7abed0b1b5c4af272a4c82decd' \
