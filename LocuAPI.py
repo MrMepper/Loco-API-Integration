a11c4bdf4023b8e255aca199b57e1508acb59611
Christopher Moore <MrMepper@gmail.com>
2015-05-21 22:28:24 -0500

    Searches for name and phone number

--- LocuAPI.py
+++ LocuAPI.py
@@ -1,8 +1,9 @@
-locu_api = '4fda38a894269e7abed0b1b5c4af272a4c82decd'
+
 
 import urllib.request
 import json
 import codecs
+locu_api = '4fda38a894269e7abed0b1b5c4af272a4c82decd'
 url = 'https://api.locu.com/v1_0/venue/search/?locality=Conway&api_key=4fda38a894269e7abed0b1b5c4af\
 272a4c82decd'
 request = urllib.request.urlopen(url)
@@ -10,4 +11,5 @@ reader = codecs.getreader("utf-8")
 data = json.load(reader(request))
 
 for each in data['objects']:
-    print (each)
+    print (each['name'])
+    print (each['phone'])
